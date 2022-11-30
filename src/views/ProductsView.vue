ed27d053e3f15540374ccd6b0010909fe96b2a84
Angel Martínez <angelmanuelmartinezfernandez@gmail.com>
2022-11-30 15:45:26 +0100

    creada aproximación de la vista ProductsView.vue

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,8 +1,72 @@
 <script setup>
+import { computed, onBeforeMount, ref } from 'vue'
+import { useProductsStore } from '@/stores/products.js'
+import { useCartStore } from '@/stores/cart'
+
+const productsStore = useProductsStore()
+const cartStore = useCartStore()
+// CICLO DE VIDA DE VUE, TAMBIÉN SE CONOCEN COMO HOOKS, BUSCAR EN LA DOCUMENTACIÓN OFICIAL:
+// onBeforeMount: AQUI SUELO TRATAR DATOS ANTES DE QUE SE RENDERICE EL TEMPLATE (HTML)
+// onMounted: ES LA PARTE "FINAL" EN LA QUE SE RENDERIZA EL TEMPLATE (HTML)
+// onBeforeUnmount: AQUI SUELO TRATAR ACCIONES QUE SE REALIZAN AL DESTRUIR EL COMPONENTE
+// SI TENÉIS DUDAS PONERLOS TODOS CON UN CONSOLE.LOG DIFERENTE EN CADA UNO, ASI VERÉIS EL ORDEN.
+onBeforeMount( () => {
+  getProducts()
+})
+
+// DATA
+const loading = ref(true)
+
+// COMPUTED
+const loadingClass = computed(() => {
+  return loading.value ? 'loading' : ''
+})
+
+// METHODS
+const getProducts = async () => {
+  await productsStore.fetchProducts()
+  loading.value = false
+}
+
+const addProduct = (productId) => {
+  const product = productsStore.getProductById(productId)
+  cartStore.addItemToShoppingCart(product)
+}
 </script>
 
 <template>
-  <main>
-    <h1>Products Template</h1>
-  </main>
+  <div id="home">
+    <div class="content" :class="loadingClass">
+      <div v-if="loading" class="lds-dual-ring"></div>
+      <!--      LOADING SACADO DE https://loading.io/css/-->
+      <div class="content__products" v-else v-for="product in productsStore.products" :key="product.id">
+        {{product.title}} {{product.price}}
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+#home {
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.content__products {
+  width: 90%;
+}
+
+.loading {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
