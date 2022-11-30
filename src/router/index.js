b50abbb6174d6b4bfd48ce692fa20733e80cfe56
AngelMartinezFernandez <76613677+AngelMartinezFernandez@users.noreply.github.com>
2022-11-30 13:06:20 +0100

    initial proyect

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Sidebar from '../components/Sidebar.vue'
+import ProductsView from '../views/ProductsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'products-view',
+      components: {
+        default: ProductsView,
+        aside: Sidebar
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Products Template</h1>
+  </main>
+</template>
